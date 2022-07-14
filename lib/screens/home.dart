0dcbe895b07f0d5d9ffc16d56fd086a877a8d46c
luismhenriquez <luismhenriqueztv@gmail.com>
2022-07-13 22:28:40 -0400

    primer commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeApp extends StatelessWidget {
+  const HomeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.brown,
+        actions: [
+          Row(
+            children: [
+              IconButton(
+                icon: const Icon(
+                  Icons.arrow_back_ios_new,
+                ),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: const Icon(Icons.notifications),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
